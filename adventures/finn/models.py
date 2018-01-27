b9a984f1132b4744163e4a20fc9caf333311166e
NarutoMatvey <NarutoMatvey@mail.ru>
2018-01-27 12:21:29 +0300

    Второй урок

--- adventures/finn/models.py
+++ adventures/finn/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Episodes(models.Model):
+	title_episod = models.CharField(max_length=120)
+	description_episode = models.TextField()
+	release_episode = models.DateTimeField('date release')
